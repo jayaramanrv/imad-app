b835c4c85d123df5110c348c3dfde3c90894b56f
jayaramanrv <rvjayaraman@gmail.com>
2018-03-03 10:46:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,8 +76,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send(createTemplate(articleOne));
+app.get('/:articleName', function (req, res) {
+  res.send(createTemplate(articlename));
 });
 
 app.get('/article-two', function (req, res) {
